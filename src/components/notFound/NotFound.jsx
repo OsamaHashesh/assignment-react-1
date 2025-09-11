962a17dd655e8c5b69d4971989e0a48862b9f711
OsamaHashesh <osamahashesh3@gmail.com>
2025-09-11 15:11:05 +0300

    edit

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import img4 from "../../assets/images/notfound.jpg";
 export default function NotFound() {
   return (
     <div className=''>
-      <img className='w-100 h-100' src="images\notfound.jpg" alt="not found" />
+      <img className='w-100 h-100' src={img4} alt="not found" />
     </div>
   )
 }
